050b4e78af9a94dd73d386ff41d3723a2ae3088e
Saurav Singh <68386469+Saurav-singh06@users.noreply.github.com>
2022-01-04 16:45:54 +0530

    Update Binary_Search_04.cpp

--- Array/Binary_Search_04.cpp
+++ Array/Binary_Search_04.cpp
@@ -1,27 +1,33 @@
-
-#include <iostream>
+#include<iostream>
 using namespace std;
+	
 
-void binary_search(int arr[],int n,int key){
-    int s=0;
-    int e=n-1;
-    
-    while(s<=e){
-        int mid = (s+e)/2;
+int binary_search(int arr[],int n,int key){
+	
+	int s = 0;
+	int e = n - 1;
 
-        if(arr[mid]==key){
-            return mid;
-        }
-        else if(arr[mid]>key){
-            e=mid-1;
-        }else{
-            s=mid+1
-        }
-    }
-    return -1;
+	while(s<=e){
+		int mid = (s+e)/2;
+
+		if(arr[mid] == key){
+			return mid;
+		}
+		else if(arr[mid] > key){
+			e = mid - 1;
+		}
+		else{
+
+			s = mid + 1;
+		}
+	}
+
+	return -1;
 }
 
-int main() {
+
+int main(){
+
 	int arr[] = {10,20,30,40,45,60,70,89};
 	int n = sizeof(arr)/sizeof(int);
 
@@ -35,6 +41,7 @@ int main() {
 	else{
 		cout<<key <<" is NOT Found!" <<endl;
 	}
+
+
 	return 0;
 }
-
