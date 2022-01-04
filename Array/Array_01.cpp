a754e2f35aa788bc24dcbe08fc04ba883d63457c
Saurav Singh <68386469+Saurav-singh06@users.noreply.github.com>
2022-01-04 15:09:24 +0530

    Create Array_01.cpp

---
+++ Array/Array_01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int arr[100];
+	int n;
+
+	cout<<"Enter the nos of student:  ";
+	cin>>n;
+
+	//Input 
+	for(int i=0; i<n; i++){
+		cin>> arr[i];
+	}
+
+	//output
+	for(int i=0; i<n; i++){
+		cout<< arr[i]<<" ";
+	}
+		cout<<endl;
+return 0;
+}
